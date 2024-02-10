a8cd522971a3a9d7efb8e5921e0b6013e6a76b7f
Coppermine <229118171+Coppermine-factorio@users.noreply.github.com>
2024-02-10 05:06:14 -0500

    Make pipes less wiggly
    By rearranging the order in which neighbours are searched we get neater
    pipes.

--- control.lua
+++ control.lua
@@ -1,8 +1,8 @@
 local neighbours = {
-  { x = 0, y = -1 },
-  { x = 0, y = 1 },
   { x = -1, y = 0 },
+  { x = 0, y = 1 },
   { x = 1, y = 0 },
+  { x = 0, y = -1 },
 }
 
 local function MoreThanOne(t)
