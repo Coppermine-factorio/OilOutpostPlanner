9bfc27d66ec104813037bf26d8f95d994e0dc3c1
Coppermine <229118171+Coppermine-factorio@users.noreply.github.com>
2025-01-28 16:17:12 -0500

    Extract version from info.json
    Previously the release script would get the version number from the
    directory name, but that was inconvenient because it required renaming
    the directory often.
    
    Now we instead take it from the info.json, so the information is not
    duplicated.

--- release.sh
+++ release.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 cd $(dirname $0)
-release_name=$(basename $PWD)
+version=$(jq -r .version info.json)
+dirname=$(basename $PWD)
+release_name=${dirname}_$version
 full_release_path=$HOME/projects/factorio/factorio-mods/$release_name.zip
 
 if [ -e "$full_release_path" ]
 then
-  printf "Release file already exists\n" >&2
+  printf "Release file '%s' already exists\n" "$full_release_path" >&2
   exit 1
 fi
 
 cd ..
 
-swap_files=$(find $release_name -name '*.sw[po]')
+swap_files=$(find $dirname -name '*.sw[po]')
 
-zip -r "$full_release_path" $release_name/[^.rT]* ${swap_files:+-x} $swap_files
+zip -r "$full_release_path" $dirname/[^.rT]* ${swap_files:+-x} $swap_files
